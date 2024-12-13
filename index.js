297c5f7112d5bc240103dcae9d29176703290714
dev-reworkacademy <132952009+dev-reworkacademy@users.noreply.github.com>
2024-12-13 12:09:54 +0100

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const app = express();
 const PORT = 8000;
 
+
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
